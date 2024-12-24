7516efc91156d147eb558a623f5284677d4e7dd4
miker <mikekibbe73@gmail.com>
2024-12-24 13:36:15 -0800

    Add name variable and greeting functionality

---
+++ DEC22/name.py
@@ -0,0 +1,11 @@
+name = "ada lovelace"
+#This code snippet defines a variable called name and assigns it the value "ada lovelace".
+first_name = "ada"
+last_name = "lovelace"
+#This code snippet defines two variables called first_name and last_name and assigns them the values "ada" and "lovelace" respectively.
+full_name = f"{first_name} {last_name}"
+print(full_name)
+#This code snippet uses f-string formatting to combine the values of the first_name and last_name variables into a single string and assigns it to the full_name variable.
+print("Hello, " + full_name.title() + "!")
+#This code snippet uses string concatenation to create a greeting message that includes the value of the full_name variable.
+
