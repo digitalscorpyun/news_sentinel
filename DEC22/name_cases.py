d1d096e5435b810c944916b078a8a63fffe81c41
miker <mikekibbe73@gmail.com>
2024-12-25 21:31:34 -0800

    29

---
+++ DEC22/name_cases.py
@@ -0,0 +1,4 @@
+name = "Frederick"
+message= "Hello " + name + " how are you?"
+print(message)
+# The name of a person is stored in a variable called name. The variable name is then used in a message that is printed to the screen.
