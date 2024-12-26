af45bf0ba43b3e06a24abc97e03458e2287c08bf
miker <mikekibbe73@gmail.com>
2024-12-25 23:54:33 -0800

    digital_scorpyun 2024-12-25 11:55:00

---
+++ DEC22/bicycles.py
@@ -0,0 +1,12 @@
+# Desc: This program demonstrates how to work with lists in Python
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+# Accessing the first element in the list
+print(bicycles[0])
+# Accessing an element in the list and using the title() method to capitalize the first letter
+print(bicycles[0].title())
+# Accessing multiple elements in the list
+print(bicycles[1])
+print(bicycles[3])
+# Accessing the last element in the list
+print(bicycles[-1])
