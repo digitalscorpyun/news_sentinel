fd4243e011c64ef376f484c050799ebad12efcad
miker <mikekibbe73@gmail.com>
2024-12-25 23:19:04 -0800

    digitalscorpyun 2024-12-25 11:20:00

---
+++ DEC22/comment.py
@@ -0,0 +1,3 @@
+#digitalscorpyun 2024/12/25
+# Say hello to everyone.
+print("Hello Python people!")
---
+++ DEC22/name_cases2.py
@@ -0,0 +1,11 @@
+message="The limits of tyrants are prescribed by the endurance of those whom they oppress."
+print(message)
+famous_person=" Frederick Douglass "
+print(famous_person)
+print("\t Frederick Douglass ")
+print("\n Frederick Douglass ")
+quote=famous_person+" once said, "+message
+print(quote)
+print(famous_person.lstrip())
+print(famous_person.rstrip())
+print(famous_person.strip())
